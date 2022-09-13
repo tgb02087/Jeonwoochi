2ba4533f7d99c6969bafcae5057c6b3c89252711
김성찬 <kimsc0714@naver.com>
2022-09-13 16:37:29 +0900

    ✨ feat(style): import minireset css file

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import './styles/minireset.min.css';
 import './styles/fonts.css';
 
 if (process.env.NODE_ENV === 'development') {
