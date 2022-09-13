5e262193fda88e365f5484a7c6d04960780f31bf
김성찬 <kimsc0714@naver.com>
2022-09-13 17:31:09 +0900

    ✨ feat(style): add global theme

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 import Main from './components/pages/Main';
 import Interest from './components/pages/Interest';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/interest" element={<Interest />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/interest" element={<Interest />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
