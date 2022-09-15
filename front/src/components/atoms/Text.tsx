be6661ff0bc9d7c43c7ad4587ab9e4e62e8466b7
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-09-15 10:21:24 +0900

    📝 docs(text_component): update docs for Text.tsx

--- front/src/components/atoms/Text.tsx
+++ front/src/components/atoms/Text.tsx
@@ -15,6 +15,7 @@ const StyledText = styled.div`
  *
  * @description
  * message props에 텍스트를 적어 보낸다. DungGeunMo 폰트가 자동 적용된다.
+ * clickHandler 를 통해 클릭 이벤트 함수르 호출하는 것이 가능하다.
  * @example
  * <Test message={"나 아이폰 샀음"}/>
  *
