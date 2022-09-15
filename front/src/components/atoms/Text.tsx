47a5ed03e95746613dd8d1e975cbe1c5e26bb0d2
김성찬 <kimsc0714@naver.com>
2022-09-15 16:47:56 +0900

    ✨ feat(text): add text color prop

--- front/src/components/atoms/Text.tsx
+++ front/src/components/atoms/Text.tsx
@@ -5,24 +5,30 @@ import tw from 'twin.macro';
 interface PropTypes {
   clickHandler?: React.MouseEventHandler<HTMLDivElement>;
   message: string;
+  color?: string;
 }
 
 const StyledText = styled.div`
   ${tw`font-DungGeunMo`}
+  ${({ color }) => (color ? `color: ${color};` : tw`text-black`)}
 `;
 
 /**
  *
  * @description
- * message props에 텍스트를 적어 보낸다. DungGeunMo 폰트가 자동 적용된다.
+ * message props에 텍스트를 적어 보낸다. 검정색 DungGeunMo 폰트가 자동 적용된다.
  * clickHandler 를 통해 클릭 이벤트 함수르 호출하는 것이 가능하다.
  * @example
  * <Test message={"나 아이폰 샀음"}/>
  *
  */
 
-const Text = ({ message, clickHandler }: PropTypes) => {
-  return <StyledText onClick={clickHandler}>{message}</StyledText>;
+const Text = ({ message, clickHandler, color }: PropTypes) => {
+  return (
+    <StyledText onClick={clickHandler} color={color}>
+      {message}
+    </StyledText>
+  );
 };
 
 export default Text;
