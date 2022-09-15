313e70d09dc6a282cec4d67ec6e93edb77027f2b
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-09-16 02:15:57 +0900

    ✨ feat(mapapi): setting logic for kakao map api

--- front/src/components/atoms/KakaoMap.tsx
+++ front/src/components/atoms/KakaoMap.tsx
@@ -7,6 +7,14 @@ interface PropTypes {
   };
 }
 
+/**
+ * @description
+ * lat, lng 기준으로
+ * 카카오 맵 API 렌더링 예정
+ *
+ * @author bell
+ */
+
 const KakaoMap = ({ coord }: PropTypes) => {
   const { lat, lng } = coord;
   return (
