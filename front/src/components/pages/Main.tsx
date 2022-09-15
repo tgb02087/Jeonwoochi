486077967fa8f24da0a6e47e4a8acf14842aa11a
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-09-15 16:50:10 +0900

    ✨ feat(react-query): setting react-query
    setting react-query & setting msw server(festival)

--- front/src/components/pages/Main.tsx
+++ front/src/components/pages/Main.tsx
@@ -1,21 +1,9 @@
-import React, { useEffect } from 'react';
-
-import { useQuery } from 'react-query';
-import axios from 'axios';
-import { MapData } from '../../recoil/atoms';
-
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 const Main = () => {
-  useQuery<MapData[]>(['Maps'], async () => {
-    const response = await axios.get('/maps');
-    return response.data;
-  });
-
   return (
     <>
       <div>main</div>
-      <Link to={'/map/2'}> map 페이지</Link>
     </>
   );
 };
--- front/src/mocks/handlers.ts
+++ front/src/mocks/handlers.ts
@@ -1,4 +1,3 @@
 import mockTest from './handlers/mock-test';
-import maps from './handlers/maps';
 
-export const handlers = [mockTest, maps];
+export const handlers = [mockTest];
