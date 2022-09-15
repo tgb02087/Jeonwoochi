cbd75824b03eb04ed58e416becc27fd9de2dd3c0
seonghun <pch468@naver.com>
2022-09-15 13:47:19 +0900

    💎 style(theme.ts): add flex rowCenter and columnCenter

--- front/src/styles/theme.ts
+++ front/src/styles/theme.ts
@@ -1,9 +1,24 @@
 const color = {
   primaryColor: 'blue',
 };
+const flex = {
+  rowCenter: `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `,
+  columnCenter: `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+  `,
+};
 
 export const theme = {
   color,
+  flex,
 };
 
 export type Theme = typeof theme;
