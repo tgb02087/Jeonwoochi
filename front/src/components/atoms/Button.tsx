4168c85a2c9c436e100c3e545755bb6edca2a34f
김성찬 <kimsc0714@naver.com>
2022-09-15 16:47:34 +0900

    ✨ feat(button): add background color prop

--- front/src/components/atoms/Button.tsx
+++ front/src/components/atoms/Button.tsx
@@ -6,15 +6,18 @@ interface PropTypes {
   clickHandler?: React.MouseEventHandler<HTMLButtonElement>;
   children: JSX.Element;
   isText: boolean;
+  color?: string;
 }
 
 const StyledButtonText = styled.button`
-  ${tw`inline bg-gray-200 hover:bg-gray-300 hover:text-white py-2 px-4 rounded-lg`}
+  ${tw`flex gap-3 items-center py-2 px-4 rounded-lg`}
+  ${({ color }) => (color ? `background-color: ${color};` : tw`bg-gray-300`)}
 
   border-bottom: 4px solid #00000044;
 
   &:hover {
     border-bottom: 5px solid #00000044;
+    filter: brightness(90%);
     transition: all 0.1s;
   }
 
@@ -25,12 +28,14 @@ const StyledButtonText = styled.button`
 `;
 
 const StyledButtonIcon = styled.button`
-  ${tw`inline bg-gray-200 hover:bg-gray-300 hover:text-white py-2 px-2 rounded-full`}
+  ${tw`flex gap-3 items-center py-2 px-2 rounded-full`}
+  ${({ color }) => (color ? `background-color: ${color};` : tw`bg-gray-300`)}
 
   border-bottom: 4px solid #00000044;
 
   &:hover {
     border-bottom: 5px solid #00000044;
+    filter: brightness(90%);
     transition: all 0.1s;
   }
 
@@ -53,13 +58,21 @@ const StyledButtonIcon = styled.button`
  *    {<Text message="아이폰 13" />}
  * </Button>
  *
- * @author bell
  */
-const Button = ({ clickHandler, isText, children }: PropTypes): JSX.Element => {
+const Button = ({
+  clickHandler,
+  isText,
+  children,
+  color,
+}: PropTypes): JSX.Element => {
   return isText ? (
-    <StyledButtonText onClick={clickHandler}>{children}</StyledButtonText>
+    <StyledButtonText onClick={clickHandler} color={color}>
+      {children}
+    </StyledButtonText>
   ) : (
-    <StyledButtonIcon onClick={clickHandler}>{children}</StyledButtonIcon>
+    <StyledButtonIcon onClick={clickHandler} color={color}>
+      {children}
+    </StyledButtonIcon>
   );
 };
 
