649aa8ea99c67b4f160d25603564384c733209ed
seonghun <pch468@naver.com>
2022-09-19 00:24:20 +0900

    ✨feat(main): add main page UI icons and Festival Sidebar

--- front/src/components/pages/Main.tsx
+++ front/src/components/pages/Main.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
+import Alert from '../../icons/Alert';
+import Profile from '../../icons/Profile';
+import Question from '../../icons/Question';
+import Button from '../atoms/Button';
+import Text from '../atoms/Text';
+import FestivalSideBar from '../organisms/FestivalSideBar';
 
 const Main = () => {
+  // opened 여부에 따라 화살표 방향, display none 바꿔주기
+  const [openedSideBar, setOpenedSideBar] = useState(true);
+  const clickHandler = () => {
+    setOpenedSideBar(prev => !prev);
+  };
   return (
     <>
-      <div>main</div>
+      <Button isText={false}>
+        <Profile />
+      </Button>
+      <Button isText={false}>
+        <Question />
+      </Button>
+      <Button isText={false}>
+        <Alert />
+      </Button>
+      <Button isText={true}>
+        <Text message={'축제 요청'} />
+      </Button>
+
+      <FestivalSideBar
+        openedSideBar={openedSideBar}
+        clickHandler={clickHandler}
+      />
     </>
   );
 };
