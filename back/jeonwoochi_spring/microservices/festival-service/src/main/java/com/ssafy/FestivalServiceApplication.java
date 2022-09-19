3c87c39c634c5f9b750672f2ed08d869a5d80740
석재호 <wogh9705@naver.com>
2022-09-19 20:55:34 +0900

    Update back/jeonwoochi_spring/microservices/festival-service/src/main/java/com/ssafy/FestivalServiceApplication.java, back/jeonwoochi_spring/spring-cloud/gateway-service/src/main/java/com/ssafy/GatewayServiceApplication.java

--- back/jeonwoochi_spring/microservices/festival-service/src/main/java/com/ssafy/FestivalServiceApplication.java
+++ back/jeonwoochi_spring/microservices/festival-service/src/main/java/com/ssafy/FestivalServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FestivalServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FestivalServiceApplication.class, args);
