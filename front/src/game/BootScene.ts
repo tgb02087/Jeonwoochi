51be3734ee9239c537443c896399de79cb185e8e
김성찬 <kimsc0714@naver.com>
2022-09-20 16:34:19 +0900

    ✨ feat(phaser): create game view component

---
+++ front/src/game/BootScene.ts
@@ -0,0 +1,15 @@
+import { Scene } from 'phaser';
+
+/**
+ * 게임 씬(Scene) 관리 클래스
+ *
+ * @author Sckroll
+ */
+class BootScene extends Scene {
+  // preload() {}
+  // init() {}
+  // create() {}
+  // resize() {}
+}
+
+export default BootScene;
