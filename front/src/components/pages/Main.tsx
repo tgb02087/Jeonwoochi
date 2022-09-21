3712ab93889f06e27f7a299e55106b039d62c5bc
seonghun <pch468@naver.com>
2022-09-21 10:19:24 +0900

    ✨ feat(main): update with FestivalModal

--- front/src/components/pages/Main.tsx
+++ front/src/components/pages/Main.tsx
@@ -1,15 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
+import getFestivalItem from '../../api/getFestivalItem';
 import Alert from '../../icons/Alert';
 import Profile from '../../icons/Profile';
 import Question from '../../icons/Question';
 import Button from '../atoms/Button';
-import GameView from '../organisms/GameView';
 import Text from '../atoms/Text';
+import FestivalModal from '../organisms/FestivalModal';
 import FestivalSideBar from '../organisms/FestivalSideBar';
+import GameView from '../organisms/GameView';
 
 const Main = () => {
+  // 데이터를 초기 렌더링 시 fetch해 가지고 있다가
+  // 다른 축제 오브젝트에 접근하면 key만 다르게 해서 다시 fetch 해오기
+  const { data, isLoading, isError } = useQuery(['info'], getFestivalItem, {
+    staleTime: 1000 * 20,
+  });
   // opened 여부에 따라 화살표 방향, display none 바꿔주기
   const [openedSideBar, setOpenedSideBar] = useState(true);
+  // 아래 시작 state 나중에 false로 바꾸기
+  const [openedFestivalModal, setOpenedFestivalModal] = useState(true);
   const clickHandler = () => {
     setOpenedSideBar(prev => !prev);
   };
@@ -33,6 +43,9 @@ const Main = () => {
         openedSideBar={openedSideBar}
         clickHandler={clickHandler}
       />
+      {openedFestivalModal ? (
+        <FestivalModal setState={setOpenedFestivalModal} info={data} />
+      ) : null}
 
       <GameView />
     </>
