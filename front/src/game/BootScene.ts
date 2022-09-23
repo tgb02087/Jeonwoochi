d8b8e9c41b4dc59f2868185c6c7e3e272de482e5
김성찬 <kimsc0714@naver.com>
2022-09-23 16:42:58 +0900

    ✨ feat(phaser): show temporary festival object on map

--- front/src/game/BootScene.ts
+++ front/src/game/BootScene.ts
@@ -11,6 +11,7 @@ class BootScene extends Scene {
   private player!: Player;
   private checkCollide = false;
   private minimap!: Phaser.Cameras.Scene2D.Camera;
+  private spawns!: Phaser.Physics.Arcade.Group; // 스폰되는 축제 오브젝트들을 관리하는 멤버
 
   preload() {
     // 타일맵 불러오기
@@ -18,7 +19,10 @@ class BootScene extends Scene {
     this.load.tilemapTiledJSON('map', map);
     this.load.audio('bgm', ['/audios/bgm/2 - Big Giant Trees - Gotanda.mp3']);
 
-    // Player assets preload
+    // 축제 오브젝트 이미지 파일 불러오기
+    this.load.image('sample-object', '/images/festival/sample-object.png');
+
+    // 플레이어 클래스 불러오기
     Player.preload(this);
 
     // 더미 festival atlas
@@ -127,6 +131,26 @@ class BootScene extends Scene {
     // 미니맵도 캐릭터에 포커싱 되도록
     const minimapCamera = this.cameras.cameras.find(el => el.name === 'mini');
     minimapCamera?.startFollow(this.player.me);
+
+    // 축제 오브젝트 생성
+    this.createFestivalObjects();
+  }
+
+  /**
+   * 축제 오브젝트를 생성하는 메소드
+   */
+  createFestivalObjects() {
+    this.spawns = this.physics.add.group({
+      classType: Phaser.GameObjects.Sprite,
+    });
+
+    // 오브젝트 생성 테스트
+    const festival = this.spawns.create(
+      32 * 295 + 16,
+      32 * 178 + 16,
+      'sample-object',
+    );
+    festival.body.setImmovable();
   }
 
   update() {
