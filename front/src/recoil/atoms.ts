b4b57f974dfaef635d6f3d45b318733519c65fdd
김성찬 <kimsc0714@naver.com>
2022-09-25 00:35:41 +0900

    ✨ feat(recoil): initialize recoil

--- front/src/recoil/atoms.ts
+++ front/src/recoil/atoms.ts
@@ -1,1 +1,12 @@
 import { atom } from 'recoil';
+
+export const festivalList = atom({
+  key: 'festivalList',
+  default: [
+    {
+      name: 'test',
+      lan: 36,
+      lon: 126,
+    },
+  ],
+});
