5e4dac6ddb215ccceda9ccd0e5bdbd80969de5e5
김성찬 <kimsc0714@naver.com>
2022-09-27 22:46:30 +0900

    ✨ feat(phaser): create sample festival object generation call by eventEmitter

--- front/src/game/BootScene.ts
+++ front/src/game/BootScene.ts
@@ -12,10 +12,10 @@ import eventEmitter from '../utils/eventEmitter';
  */
 class BootScene extends Scene {
   private player!: Player;
-  private festivalObject!: Resource;
   private checkCollide = false;
   private minimap!: Phaser.Cameras.Scene2D.Camera;
-  private spawns!: Phaser.Physics.Arcade.Group; // 스폰되는 축제 오브젝트들을 관리하는 멤버
+  private festivalList!: MapData[] | undefined;
+  private festivalListFetched = false;
 
   preload() {
     // 타일맵 불러오기
@@ -23,14 +23,17 @@ class BootScene extends Scene {
     this.load.tilemapTiledJSON('map', map);
     this.load.audio('bgm', ['/audios/bgm/2 - Big Giant Trees - Gotanda.mp3']);
 
-    // 축제 오브젝트 이미지 파일 불러오기
-    this.load.image('sample-object', '/images/festival/sample-object.png');
-
     // 플레이어 클래스 불러오기
     Player.preload(this);
 
     // 더미 festival atlas
     Resource.preload(this);
+
+    // 리액트 컴포넌트로부터 축제 리스트를 받고 저장
+    eventEmitter.on('festivals', (festivalList?: MapData[]) => {
+      this.festivalList = festivalList;
+      this.festivalListFetched = true;
+    });
   }
 
   create() {
@@ -63,10 +66,10 @@ class BootScene extends Scene {
     );
 
     // 더미 festival 정보 생성
-    const festivalInfo = map.findObject(
-      'Objects',
-      obj => obj.name === 'festival',
-    );
+    // const festivalInfo = map.findObject(
+    //   'Objects',
+    //   obj => obj.name === 'festival',
+    // );
 
     // 플레이어 인스턴스
     this.player = new Player(
@@ -77,15 +80,6 @@ class BootScene extends Scene {
       'misa-front',
     );
 
-    // 페스티벌 오브젝트 인스턴스
-    this.festivalObject = new Resource(
-      this,
-      festivalInfo.x || 0,
-      festivalInfo.y || 0,
-      'festival',
-      'festival2',
-    );
-
     // this.minimap.setBackgroundColor(0x002244);
 
     // 맵 collider 설정 세팅
@@ -94,23 +88,6 @@ class BootScene extends Scene {
         console.log('바다와 접촉했다');
       }
     });
-
-    this.physics.add.collider(
-      this.player,
-      this.festivalObject.me,
-      (player, _) => {
-        // 이곳이 바로 축제별 페이지를 부르는 함수를 호출하면 된다잉
-        // 근데 어떻게 호출하지...
-        if (!player.body.checkCollision.none) {
-          console.log('축제 오브젝트와 접촉했다');
-          // console.log('축제 오브젝트와 접촉했다');
-          // setTimeout(() => {
-          //   this.checkCollide = false;
-          //   console.log('다시 false로 바귐');
-          // }, 3000);
-        }
-      },
-    );
 
     // 카메라 설정
     const camera = this.cameras.main;
@@ -138,36 +115,30 @@ class BootScene extends Scene {
     // 미니맵도 캐릭터에 포커싱 되도록
     const minimapCamera = this.cameras.cameras.find(el => el.name === 'mini');
     minimapCamera?.startFollow(this.player.me);
-
-    // 리액트 컴포넌트로부터 축제 리스트 받기
-    eventEmitter.on('festivals', (festivalList?: MapData[]) => {
-      // 축제 오브젝트 생성
-      this.createFestivalObjects(festivalList);
-    });
-  }
-
-  /**
-   * 축제 오브젝트를 생성하는 메소드
-   */
-  createFestivalObjects(festivalList?: MapData[]) {
-    this.spawns = this.physics.add.group({
-      classType: Phaser.GameObjects.Sprite,
-    });
-
-    // 우선 테스트를 위해 콘솔에 출력만 수행
-    console.log(festivalList);
-
-    // 오브젝트 생성 테스트
-    const festival = this.spawns.create(
-      32 * 295 + 16,
-      32 * 178 + 16,
-      'sample-object',
-    );
-    festival.body.setImmovable();
   }
 
   update() {
     this.player.update();
+
+    // festivalListFetched 상태로 업데이트 여부 확인
+    if (this.festivalListFetched) {
+      this.createFestivalObjects();
+      this.festivalListFetched = false;
+    }
+  }
+
+  /**
+   * 위도 및 경도에 따라 축제 오브젝트를 생성하는 메소드
+   */
+  createFestivalObjects() {
+    // 페스티벌 오브젝트 인스턴스
+    const { me } = new Resource(this, 9664.5, 5727, 'festival', 'festival2');
+
+    this.physics.add.collider(this.player, me, player => {
+      if (!player.body.checkCollision.none) {
+        console.log('축제 오브젝트와 접촉했다');
+      }
+    });
   }
 }
 
