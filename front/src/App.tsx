d7ae80679362793354e96cac63d6986d184cd47f
김성찬 <kimsc0714@naver.com>
2022-09-30 16:46:03 +0900

    ✨ feat(route): redirect route by login(auth) state

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
-
 import { theme } from './styles/theme';
 import Main from './components/pages/Main';
 import Login from './components/pages/Login';
@@ -11,6 +11,7 @@ import MapAPI from './components/pages/MapAPI';
 
 const App = () => {
   const queryClient = new QueryClient();
+  const [isLogin, setIsLogin] = useState(false); // 임시로 로그인 여부를 false 처리
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -18,8 +19,19 @@ const App = () => {
         <ThemeProvider theme={theme}>
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Main />}></Route>
+              {isLogin ? (
+                <Route
+                  path="/"
+                  element={<Navigate replace to="/game" />}
+                ></Route>
+              ) : (
+                <Route
+                  path="/"
+                  element={<Navigate replace to="/login" />}
+                ></Route>
+              )}
               <Route path="/login" element={<Login />}></Route>
+              <Route path="/game" element={<Main />}></Route>
               <Route path="/interest" element={<Interest />}></Route>
               <Route path="/map/:id" element={<MapAPI />}></Route>
             </Routes>
