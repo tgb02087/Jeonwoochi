3c8e6642ac3b5cd285e53fe2e2254fca87c8921b
beberiche <woaol@naver.com>
2022-10-01 23:12:07 +0900

    📝 docs(readme): add done list for bell 221001

--- front/src/components/atoms/KakaoMap.tsx
+++ front/src/components/atoms/KakaoMap.tsx
@@ -44,7 +44,7 @@ const InfoWindow = ({ title }: PropTypesInfoWindow): JSX.Element => (
 
 /**
  * @description
- * EventMarkerContainer의
+ * EventMarkerContainer의 Props 타입 지정
  *
  * @author bell
  */
@@ -57,6 +57,15 @@ interface PropTypesEventMarkerContainer {
   markerSrc: string;
 }
 
+/**
+ * @description
+ * 이벤트를 적용한 마커를 생성하는 컴포넌트
+ *
+ * @param position {object} lat, lng를 값을 담은 객체
+ * @param markerSrc {string} 해당하는 마커 이미지의 경로
+ *
+ * @author bell
+ */
 const EventMarkerContainer = ({
   position,
   markerSrc,
@@ -67,78 +76,12 @@ const EventMarkerContainer = ({
     <MapMarker
       position={{ lat: position.lat, lng: position.lng }}
       onClick={() => setIsVisible(prev => !prev)}
-      // image={{ src: markerSrc }}
       image={{ src: markerSrc, size: { width: 50, height: 50 } }}
-      // onMouseOver={() => setIsVisible(true)}
-      // onMouseOut={() => setIsVisible(false)}
     >
       {isVisible && <InfoWindow title={'천복 순대국밥 궁동점'} />}
     </MapMarker>
   );
 };
-
-// declare global {
-//   interface Window {
-//     kakao: any;
-//   }
-// }
-
-// // kakaoMap instance를 담아두기 위한 전역객체
-// let map: any = undefined;
-// // let marker: any = undefined;
-
-// /**
-//  *
-//  * @description
-//  * 카카오 맵의 디폴트 맵 설정을 불러오는 호출하는 함수
-//  * 해당 프로젝트의 디폴트 맵 설정은,
-//  * 축제의 좌표정보와, 축제 위치를 가리키는 캐릭터 마킹 이미지
-//  *
-//  * @author bell
-//  */
-// const mapInitHandler = (lat: number, lng: number): void => {
-//   try {
-//     const container = document.getElementById('map');
-//     const center = new window.kakao.maps.LatLng(lat, lng);
-//     const options = {
-//       center,
-//       // 서버 추천 데이터 범위 설정에 맞게 재설정할 예정
-//       // 3 기본값
-//       level: 3,
-//     };
-
-//     // 맵 렌더링
-//     map = new window.kakao.maps.Map(container, options);
-
-//     // 마커 이미지 설정
-//     // 해당 프로젝트의 마킹 이미지는 "캐릭터"이다
-//     const imageSrc = '/images/map/mira.gif';
-//     const imageSize = new window.kakao.maps.Size(32, 45);
-
-//     const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
-
-//     // 마커 생성
-//     const marker = new window.kakao.maps.Marker({
-//       map,
-//       image: markerImage,
-//       position: center,
-//     });
-
-//     // 마커 맵 상에 나타내기
-//     marker.setMap(map);
-//     // 캐릭터 마커에 한하여
-//     // 드래그 설정을 가능하게 했다.
-//     // 추후에, 드래그 이벤트를 통해, 드래그 한 지역의 위치정보를
-//     // 알아내는 이벤트를 추가해보려고 이렇게 설정함
-//     marker.setDraggable(true);
-//   } catch (err) {
-//     throw new Error('카카오 맵을 불러오지 못했습니다!');
-//   }
-// };
-
-// const markerClickHandler = () => {
-
-// }
 
 interface PropTypes {
   coord: {
