c93188b49faa22ce979dab12d72f7492d1e5df13
김성찬 <kimsc0714@naver.com>
2022-10-02 05:01:22 +0900

    ✨ feat(auth): check login state and redirect

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,46 +1,17 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+import RouteWrapper from './RouteWrapper';
 import { theme } from './styles/theme';
-import Main from './components/pages/Main';
-import Login from './components/pages/Login';
-import LoginKakaoRedirect from './components/pages/LoginKakaoRedirect';
-import Interest from './components/pages/Interest';
-import MapAPI from './components/pages/MapAPI';
 
 const App = () => {
   const queryClient = new QueryClient();
-  const [isLogin, setIsLogin] = useState(false); // 임시로 로그인 여부를 false 처리
 
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <Routes>
-              {isLogin ? (
-                <Route
-                  path="/"
-                  element={<Navigate replace to="/game" />}
-                ></Route>
-              ) : (
-                <Route
-                  path="/"
-                  element={<Navigate replace to="/login" />}
-                ></Route>
-              )}
-              <Route path="/login" element={<Login />}></Route>
-              <Route
-                path="/login/kakao"
-                element={<LoginKakaoRedirect />}
-              ></Route>
-              <Route path="/game" element={<Main />}></Route>
-              <Route path="/interest" element={<Interest />}></Route>
-              <Route path="/map/:id" element={<MapAPI />}></Route>
-            </Routes>
-          </BrowserRouter>
+          <RouteWrapper />
         </ThemeProvider>
       </RecoilRoot>
     </QueryClientProvider>
