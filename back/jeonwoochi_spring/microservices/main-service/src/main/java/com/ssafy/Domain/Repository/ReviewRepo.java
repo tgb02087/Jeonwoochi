fbc111b0dbc3cc6ea5f4673836e4aed010f6f75f
석재호 <wogh9705@naver.com>
2022-10-11 09:07:26 +0900

    ✨ feat(main-service): review by me add

--- back/jeonwoochi_spring/microservices/main-service/src/main/java/com/ssafy/Domain/Repository/ReviewRepo.java
+++ back/jeonwoochi_spring/microservices/main-service/src/main/java/com/ssafy/Domain/Repository/ReviewRepo.java
@@ -5,8 +5,10 @@ import com.ssafy.Dto.Response.ReviewResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepo extends JpaRepository<Review, Long> {
 
     List<Review> findByRestaurantId(Long restaurantId);
+    Optional<Review> findTopByRestaurantIdAndUserIdOrderByIdDesc(Long restaurantId, Long userId);
 }
